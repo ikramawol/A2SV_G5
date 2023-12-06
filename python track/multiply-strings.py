7376e77cf5f427ce5050c1aa27f58db823ef994b
Ikram Awol <99601469+ikramawol@users.noreply.github.com>
2023-12-06 16:31:47 +0300

    Added solution for Multiply Strings

---
+++ python track/multiply-strings.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        nums = []
+        nums. append(int(num1))
+        nums .append(int(num2))
+        ans = ""
+        ans += str(nums[0]*nums[1])
+        return ans 
+        
