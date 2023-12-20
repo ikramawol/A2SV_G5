70cfc789dc66cafbd24aab519ce98a9063aa5d7c
Ikram Awol <99601469+ikramawol@users.noreply.github.com>
2023-12-20 19:12:36 +0300

    Added solution for Transpose Matrix

---
+++ python track/transpose-matrix.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def transpose(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        result=[]
+        
+        for i in range(len(matrix[0])):
+            temp = []
+            for j in range(len(matrix)):
+                temp.append(matrix[j][i])
+            result.append(temp)
+        return result
