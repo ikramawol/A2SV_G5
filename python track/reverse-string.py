6516f7683c406ced0829af3053bcc099c84d0882
Ikram Awol <99601469+ikramawol@users.noreply.github.com>
2024-01-04 01:26:16 +0300

    Added solution for Reverse String

--- python track/reverse-string.py
+++ python track/reverse-string.py
@@ -6,8 +6,6 @@ class Solution:
         l = 0
         r = len(s) - 1
         while l < r:
-            temp = s[l]
-            s[l] = s[r]
-            s[r] = temp
+            s[l], s[r] = s[r], s[l]
             l += 1
             r -= 1
