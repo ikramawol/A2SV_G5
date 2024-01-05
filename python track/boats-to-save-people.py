fbfebe6dd5262f887053fe5e346702225e2dccdf
Ikram Awol <99601469+ikramawol@users.noreply.github.com>
2024-01-06 00:46:03 +0300

    Added solution for Boats to Save People

--- python track/boats-to-save-people.py
+++ python track/boats-to-save-people.py
@@ -1,5 +1,6 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
+
         people.sort()
         l, r = 0, len(people)-1
         count = 0
@@ -13,20 +14,6 @@ class Solution:
                 if people[r] <= limit:
                     count += 1
                 r -= 1
-            #print(people[r])
+                
         return count
-        """while l <= r:
-            if people[l] + people[r] > limit:
-                
-                    count += 1
-                r -= 1
-            elif people[l] + people[r] > limit and people[r] < limit:
-                
-                r -= 1
-            elif people[l] + people[r] <= limit:
-                count += 1
-                l += 1
-                r -= 1
-        return count"""
-            
-
+        
